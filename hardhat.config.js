d7b17981b2d26acdc2a4fe9ddf82dd194d29f1b4
solidity12 <volibear1001@gmail.com>
2025-02-09 16:19:34 +0900

    feat: update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+require("dotenv").config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    compilers: [{
+      version: "0.8.28",
+      settings: {
+        viaIR: true,
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        }
+      }
+    },],
+  },
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://arbitrum-sepolia-rpc.publicnode.com",
+      },
+      chainId: 421614,
+    },
+    arbtest: {
+      url: "https://arbitrum-sepolia-rpc.publicnode.com",
+      chainId: 421614,
+      accounts: [process.env.PK],
+    },
+  },
 };
